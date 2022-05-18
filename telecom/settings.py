f8e379936d567aad07cd5a226fca7c554088bd09
Sergey Vinokurov <79200711112@yandex.ru>
2022-05-18 17:47:52 +0300

    local

--- telecom/settings.py
+++ telecom/settings.py
@@ -72,16 +72,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'telecom.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'requests_db',
+        'USER': 'postgres',
+        'PASSWORD': 'ylhio65v',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
@@ -150,3 +161,4 @@ EMAIL_HOST_USER = '79200711112@yandex.ru'
 EMAIL_HOST_PASSWORD = 'unN-uWw-y7U-VLB'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
